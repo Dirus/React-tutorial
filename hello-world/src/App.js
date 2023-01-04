c892fb4c38c7e6d17fa2c62eaa4a285347718d03
Dirus <solankie97.deepesh@gmail.com>
2023-01-04 12:52:51 +0530

    event handling basics

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,6 +4,8 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Message from "./components/Message"
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 function App() {
   return (
     <div className="App">
@@ -13,6 +15,8 @@ function App() {
       <Welcome name="luke"/>
       <Message />
       <Counter />
+      <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
