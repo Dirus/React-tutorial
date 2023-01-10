6f73d4cc1f137c6add63563c13a96454097c40bb
Dirus <solankie97.deepesh@gmail.com>
2023-01-10 19:20:51 +0530

    styling and css basics

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -10,20 +10,33 @@ import EventBind from './EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import PersonList from './components/PersonList';
+import Stylesheets from './components/Stylesheets';
+import InLine from './components/InLine';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 function App() {
   return (
     <div className="App">
-      {/* <Greet name="Clarke" superhero="Superman">
-        <p>This is a children prop</p>
-      </Greet>
-      <Welcome name="luke"/>
-      <Message />
-      <Counter />
-      <FunctionClick />
-      <ClassClick />
-      <EventBind />
-      <ParentComponent /> */
-      <><UserGreeting /><PersonList /></>
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {
+      // <Greet name="Clarke" superhero="Superman">
+      //   <p>This is a children prop</p>
+      // </Greet>
+      // <Welcome name="luke"/>
+      // <Message />
+      // <Counter />
+      // <FunctionClick />
+      // <ClassClick />
+      // <EventBind />
+      // <ParentComponent />
+      // <EventBind />
+      // <UserGreeting />
+      // <PersonList />
+      <>
+          <Stylesheets />
+          <InLine />
+      </>
       }
     </div>
   );
---
+++ hello-world/src/components/Stylesheets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './myStyles.css'
+function Stylesheets() {
+  return (
+    <div>
+      <h1 className='primary font-xl'>Stylesheets</h1>
+    </div>
+  )
+}
+
+export default Stylesheets
